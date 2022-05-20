6deca24a5880bc8b49de08f7d6a35ef922856a0d
Baila3 <bailaly16@gmail.com>
2022-05-20 17:36:38 -0400

    add test anf helper functions for transitions

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]); 
+  
+  function transition(New, replace = false) {
+    if (replace == true) {
+      setHistory([initial, New])
+    }
+    setMode(New)
+    history.push(New)
+  }
+  
+  function back() {
+    if (history.length > 1) {
+    history.pop()
+    setMode(history[history.length - 1])
+    }
+  }
+  return { mode, transition, back};
+}
+
