597ce35a867eba1e705f556d9d9e388c34f3b4b8
Baila3 <bailaly16@gmail.com>
2022-05-24 22:45:37 -0400

    type check to test interviewers array

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -2,6 +2,7 @@ import React from "react";
 // import classNames from "classnames";
 import "components/InterviewerList.scss";
 import InterviewerListItem from "./InterviewerListItem";
+import PropTypes from 'prop-types'; 
 
 function InterviewerList(props) {
   
@@ -27,5 +28,9 @@ function InterviewerList(props) {
     </section>
   )
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
 
 export default InterviewerList;
